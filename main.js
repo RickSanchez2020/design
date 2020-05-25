8d31be549c7345aceede82093d3640e1ee62b973
disage <krechetov52@gmail.com>
2020-05-25 12:19:32 +0300

    add slider

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-let modal = document.getElementById('modal');
-menuOpen = () =>  {
-    modal.style.height = "100vh";
-    modal.style.padding = "20px";
-}
-menuClose = () =>  {
-    modal.style.height = "0";
-    modal.style.padding = "0";
-}
+$(".slickSlider").slick({
+  centerMode: true,
+  centerPadding: "0px",
+  slidesToShow: 3,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+let modal = document.getElementById("modal");
+menuOpen = () => {
+  modal.style.height = "100vh";
+  modal.style.padding = "20px";
+};
+menuClose = () => {
+  modal.style.height = "0";
+  modal.style.padding = "0";
+};
