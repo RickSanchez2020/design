00c42aaa1dfafe081ee53d2a8ca097c6cafef177
disage <krechetov52@gmail.com>
2020-11-21 12:11:32 +0200

    add order page

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-
-
-let modal = document.getElementById("modal");
+let modal = document.getElementById('modal');
 menuOpen = () => {
-  modal.style.height = "100vh";
-  modal.style.padding = "20px";
+  modal.style.height = '100vh';
+  modal.style.padding = '20px';
 };
 menuClose = () => {
-  modal.style.height = "0";
-  modal.style.padding = "0";
+  modal.style.height = '0';
+  modal.style.padding = '0';
 };
